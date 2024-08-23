2147293d7052ed5930e5b62c1f987544b165d62c
Scott Feichter <scottfeichter@gmail.com>
2024-08-22 22:38:56 -0700

    logout tests successfull

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -46,4 +46,16 @@ router.post(
   }
 );
 
+
+router.delete(
+  '/',
+  (_req, res) => {
+    res.clearCookie('token');
+    return res.json({ message: 'success' });
+  }
+);
+
+
+
+
 module.exports = router;
