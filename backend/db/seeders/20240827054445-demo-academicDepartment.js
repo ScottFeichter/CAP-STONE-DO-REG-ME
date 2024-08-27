26995431e50371a92ef16157269976904d692ebf
Scott Feichter <scottfeichter@gmail.com>
2024-08-26 22:53:35 -0700

    departments migros models and seeds created ready to test for seeding

---
+++ backend/db/seeders/20240827054445-demo-academicDepartment.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const { academicDepartment } = require('../models');
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await User.bulkCreate([
+      {
+        name: 'Voice',
+        chair: 'Mariah Carey',
+        imageURL: null,
+      },
+      {
+        name: 'Bowed Strings',
+        chair: 'Yoyo Ma',
+        imageURL: null,
+      },
+      {
+        name: 'Plucked Strings',
+        chair: 'Jimmy Hendrix',
+        imageURL: null,
+      },
+    ], { validate: true });
+  },
+
+  async down (queryInterface, Sequelize) {
+    options.tableName = 'Academic Departments';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      name: { [Op.in]: ['Voice', 'Bowed Strings', 'Plucked Strings'] }
+    }, {});
+  }
+};
