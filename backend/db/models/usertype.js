503865f0889d0ec9d1d7ea20c00feb8aefeea5c8
Scott Feichter <scottfeichter@gmail.com>
2024-08-29 00:23:51 -0700

    added migro and models for students and households. need to add seeders then test. then routes then reducers then pm colleciton

--- backend/db/models/usertype.js
+++ backend/db/models/usertype.js
@@ -14,6 +14,11 @@ module.exports = (sequelize, DataTypes) => {
         // onDelete: "CASCADE",
         hooks: true
       });
+      UserType.hasMany(models.Student, {
+        foreignKey: 'userType_Id',
+        // onDelete: "CASCADE",
+        hooks: true
+      });
 
     }
   }
